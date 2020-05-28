3f1d5dae0a5e243a50e5d808bed1aa35ab5656ea
namenato <nmeusling@gmail.com>
2020-05-27 20:43:14 -0400

    add dotbot to repository

---
+++ scripts/lg-ultrawide.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xrandr --newmode "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
+xrandr --addmode HDMI-1-2 2560x1080_60.00
+xrandr --output eDP-1-1 --off --output DP-1-1 --off --output HDMI-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-2 --mode 2560x1080_60.00 --pos 1920x0 --rotate normal
+
+nitrogen --restore
+killall conky
+start_conky_live_grey
