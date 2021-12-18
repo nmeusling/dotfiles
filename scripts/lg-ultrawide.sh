7ab76366b581be410c98d954f3e6728993551d72
Natalie Menato <nmeusling@gmail.com>
2021-12-18 18:05:52 -0500

    feat: clean dotfiles repo

--- scripts/lg-ultrawide.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xrandr --newmode "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
-xrandr --addmode HDMI-2 2560x1080_60.00
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --mode 2560x1080_60.00 --pos 1920x0 --rotate normal
-
-nitrogen --restore
-killall conky
-start_conky_live_grey
